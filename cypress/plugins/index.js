8521e04f1c6baa4e7cb3887ec553ec275416308f
Marcin Janik <marcin@boclips.com>
2021-04-05 21:32:33 +0200

    Revert Revert "Remove Percy and its tests"
    -I accidentally got full percy config back whereas I only needed
    publishers test suite. Removing that config again  now

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,9 @@
 const wp = require('@cypress/webpack-preprocessor');
-let percyHealthCheck = require('@percy/cypress/task');
 
 module.exports = (on, config) => {
   const options = {
     webpackOptions: require('../../webpack.config'),
   };
   on('file:preprocessor', wp(options));
-  on('task', percyHealthCheck);
   return config
 };
