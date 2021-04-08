0a38852e02965eb39a62b45aa40af90e4d17efef
Marcin Janik <marcin@boclips.com>
2021-04-08 15:21:13 +0200

    Upgrade cypress/webpack-preprocessor to latest version
    -enfroced by vulnerability check
    -required bumping webpack version
    -and adding several dev dependencies

--- setup/Constants.ts
+++ setup/Constants.ts
@@ -1,19 +1,30 @@
-export const API_URL: string =
-  process.env.CYPRESS_API_BASE_URL || Cypress.env('API_BASE_URL');
+let isCypressLoaded = false;
+try {
+  isCypressLoaded = !!Cypress;
+} catch (e) {
+  console.log('isCypressLoaded = false');
+}
 
-export const TOKEN_URL: string =
-  process.env.CYPRESS_TOKEN_URL || Cypress.env('TOKEN_URL');
+export const OPERATOR_USERNAME: string = isCypressLoaded
+  ? Cypress.env('OPERATOR_USERNAME')
+  : process.env.CYPRESS_OPERATOR_USERNAME;
 
-export const OPERATOR_USERNAME: string =
-  process.env.CYPRESS_OPERATOR_USERNAME || Cypress.env('OPERATOR_USERNAME');
+export const OPERATOR_PASSWORD: string = isCypressLoaded
+  ? Cypress.env('OPERATOR_PASSWORD')
+  : process.env.CYPRESS_OPERATOR_PASSWORD;
 
-export const OPERATOR_PASSWORD: string =
-  process.env.CYPRESS_OPERATOR_PASSWORD || Cypress.env('OPERATOR_PASSWORD');
+export const SELECTED_VIDEOS_TEST_USERNAME: string = isCypressLoaded
+  ? Cypress.env('SELECTED_VIDEOS_TEST_USERNAME')
+  : process.env.CYPRESS_SELECTED_VIDEOS_TEST_USERNAME;
 
-export const SELECTED_VIDEOS_TEST_USERNAME: string =
-  process.env.CYPRESS_SELECTED_VIDEOS_TEST_USERNAME ||
-  Cypress.env('SELECTED_VIDEOS_TEST_USERNAME');
+export const SELECTED_VIDEOS_TEST_PASSWORD: string = isCypressLoaded
+  ? Cypress.env('SELECTED_VIDEOS_TEST_PASSWORD')
+  : process.env.CYPRESS_SELECTED_VIDEOS_TEST_PASSWORD;
 
-export const SELECTED_VIDEOS_TEST_PASSWORD: string =
-  process.env.CYPRESS_SELECTED_VIDEOS_TEST_PASSWORD ||
-  Cypress.env('SELECTED_VIDEOS_TEST_PASSWORD');
+export const API_URL: string = isCypressLoaded
+  ? Cypress.env('API_BASE_URL')
+  : process.env.CYPRESS_API_BASE_URL;
+
+export const TOKEN_URL: string = isCypressLoaded
+  ? Cypress.env('TOKEN_URL')
+  : process.env.CYPRESS_TOKEN_URL;
